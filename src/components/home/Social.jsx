7f2b8e79724acd8ba7d1dbd0dee34518c09fd377
Tannu-2001 <tannukumari749@gmail.com>
2025-11-07 12:47:05 +0530

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home_social'>
+        <a href="https://www.instagram.com/tan_nu_singh" className="home_social-icon" target='_blank'>
+            <i className='bi bi-instagram'></i>
+        </a>
+
+        <a href="https://www.facebook.com/people/Tannu-Singh" className="home_social-icon" target='_blank'>
+            <i className='bi bi-facebook'></i>
+        </a>
+        <a href="https://github.com/Tannu-2001" className="home_social-icon" target='_blank'>
+            <i className='bi bi-github'></i>
+        </a>
+    </div>
+  )
+}
+export default Social
---
+++ src/components/projects/todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDoList = () => {
+  return (
+    <div className="skills_content">
+        <h3 className="skills_title">ToDoList</h3>
+        <div className='docDiv'><img src="/todo.png" alt="ToDoList" className='docImage' /></div>
+        <div style={{margin:"5px"}}><a href="https://to-do-project-frontend.vercel.app/" target='_blank' className='demolink'>See Demo <span className='bi bi-arrow-right'></span></a></div>
+    </div>
+  )
+}
+
+export default ToDoList
